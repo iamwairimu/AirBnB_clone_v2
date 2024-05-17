13814897ed3608e578f2af0eae7c773640071cb0
iamwairimu <kmnleah@gmail.com>
2024-05-17 13:35:28 -0700

    script

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 """Define route for the root URL that returns the string"""
 @app.route("/", strict_slashes=False)
-def hello_hbnb():
+def hello_holberton():
     return "Hello HBNB!"
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
